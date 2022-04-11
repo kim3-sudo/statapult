fdb92046ea71b317c10b4a7a8b5e4f80894f6d2d
Ever Croffoot-Suede <EverCS@Evers-HS-MacBook-Air-2.local>
2022-04-11 18:01:23 -0400

    Add fractional dataset

--- src/Fractional_Analysis.R
+++ src/Fractional_Analysis.R
@@ -1,3 +1,4 @@
+library(mosaic)
 ## Fractional_Analysis.R - Statapult
 ## E. Croffoot-Suede, S. Kim
 ## 11 Apr 2022 - STAT 306 DoE
@@ -5,30 +6,48 @@
 
 statapult <- read.csv(url("https://github.com/kim3-sudo/statapult/raw/main/data/data.csv"))
 
-statapult$A = statapult$arm
-statapult$B = statapult$ball
-statapult$C = statapult$position
-statapult$D = statapult$height
-statapult$E = statapult$tilt
+#figure out indexes of fractional runs----
+statapult[(statapult$arm==-1) & (statapult$ball== -1) & (statapult$position==-1) & 
+            (statapult$height == -1) & (statapult$tilt==1),]
+statapult[(statapult$arm==1) & (statapult$ball== -1) & (statapult$position==-1) & 
+            (statapult$height == -1) & (statapult$tilt==-1),]
+statapult[(statapult$arm==-1) & (statapult$ball==1) & (statapult$position==-1) & 
+            (statapult$height == -1) & (statapult$tilt==-1),]
+statapult[(statapult$arm==1) & (statapult$ball==1) & (statapult$position==-1) & 
+            (statapult$height == -1) & (statapult$tilt==1),]
+statapult[(statapult$arm==-1) & (statapult$ball== -1) & (statapult$position==1) & 
+            (statapult$height == -1) & (statapult$tilt==-1),]
+statapult[(statapult$arm==1) & (statapult$ball== -1) & (statapult$position==1) & 
+            (statapult$height == -1) & (statapult$tilt==1),]
+statapult[(statapult$arm==-1) & (statapult$ball==1) & (statapult$position==1) & 
+            (statapult$height == -1) & (statapult$tilt==1),]
+statapult[(statapult$arm==1) & (statapult$ball== 1) & (statapult$position==1) & 
+            (statapult$height == -1) & (statapult$tilt==-1),]
+statapult[(statapult$arm==-1) & (statapult$ball== -1) & (statapult$position==-1) & 
+            (statapult$height ==1) & (statapult$tilt==-1),]
+statapult[(statapult$arm==1) & (statapult$ball== -1) & (statapult$position==-1) & 
+            (statapult$height == 1) & (statapult$tilt==1),]
+statapult[(statapult$arm==-1) & (statapult$ball== 1) & (statapult$position==-1) & 
+            (statapult$height == 1) & (statapult$tilt==1),]
+statapult[(statapult$arm==1) & (statapult$ball== 1) & (statapult$position==-1) & 
+            (statapult$height == 1) & (statapult$tilt==-1),]
+statapult[(statapult$arm==-1) & (statapult$ball== -1) & (statapult$position==1) & 
+            (statapult$height == 1) & (statapult$tilt==1),]
+statapult[(statapult$arm==1) & (statapult$ball== -1) & (statapult$position==1) & 
+            (statapult$height == 1) & (statapult$tilt==-1),]
+statapult[(statapult$arm==-1) & (statapult$ball== 1) & (statapult$position==1) & 
+            (statapult$height == 1) & (statapult$tilt==-1),]
+statapult[(statapult$arm==1) & (statapult$ball==1) & (statapult$position==1) & 
+            (statapult$height == 1) & (statapult$tilt==1),]
 
-head(statapult)
-factional_statapult <- statapult[
-  which(statapult$arm==-1 & statapult$ball== -1 & statapult$position==-1 & statapult$height == -1 & statapult$tilt==1) ,
-  which(statapult$arm==1 & statapult$ball== -1 & statapult$position==-1 & statapult$height == -1 & statapult$tilt==-1),
-  which(statapult$arm==-1 & statapult$ball==1 & statapult$position==-1 & statapult$height == -1 & statapult$tilt==-1),
-  which(statapult$arm==1 & statapult$ball==1 & statapult$position==-1 & statapult$height == -1 & statapult$tilt==1),
-  which(statapult$arm==-1 & statapult$ball== -1 & statapult$position==1 & statapult$height == -1 & statapult$tilt==-1),
-  which(statapult$arm==1 & statapult$ball== -1 & statapult$position==1 & statapult$height == -1 & statapult$tilt==1),
-  which(statapult$arm==-1 & statapult$ball==1 & statapult$position==1 & statapult$height == -1 & statapult$tilt==1),
-  which(statapult$arm==1 & statapult$ball== 1 & statapult$position==1 & statapult$height == -1 & statapult$tilt==-1),
-  which(statapult$arm==-1 & statapult$ball== -1 & statapult$position==-1 & statapult$height ==1 & statapult$tilt==-1),
-  which(statapult$arm==1 & statapult$ball== -1 & statapult$position==-1 & statapult$height == 1 & statapult$tilt==1),
-  which(statapult$arm==-1 & statapult$ball== 1 & statapult$position==-1 & statapult$height == 1 & statapult$tilt==1),
-  which(statapult$arm==1 & statapult$ball== 1 & statapult$position==-1 & statapult$height == 1 & statapult$tilt==-1),
-  which(statapult$arm==-1 & statapult$ball== -1 & statapult$position==1 & statapult$height == 1 & statapult$tilt==1),
-  which(statapult$arm==1 & statapult$ball== -1 & statapult$position==1 & statapult$height == 1 & statapult$tilt==-1),
-  which(statapult$arm==-1 & statapult$ball== 1 & statapult$position==1 & statapult$height == 1 & statapult$tilt==-1),
-  which(statapult$arm==1 & statapult$ball==1 & statapult$position==1 & statapult$height == 1 & statapult$tilt==1)]
+#create df of just 16 fractional runs----
+fractional_statapult <- subset(statapult, index==17|index==2|index==3|index==20|index==5|
+                                 index==22|index==23|index==8|index==9|index==26|index==27|
+                                 index==12|index==29|index==14|index==15|index==32,
+                  select=index:distance)
 
-factional_statapult <- statapult[
-  (statapult$arm==-1 & statapult$ball== -1 & statapult$position==-1 & statapult$height == -1 & statapult$tilt==1)%in% c("index","run","arm", "ball", "position", "distance")]
+fractional_statapult$A = fractional_statapult$arm
+fractional_statapult$B = fractional_statapult$ball
+fractional_statapult$C = fractional_statapult$position
+fractional_statapult$D = fractional_statapult$height
+fractional_statapult$E = fractional_statapult$tilt
